e1b7fff0d6b69a6c763101a224095ade4ccd2a6f
safari <sargashte118@gmail.com>
2023-06-24 12:54:45 +0330

    refactor: Update home string and link

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,30 +4,35 @@ import { Link } from 'react-router-dom';
 import { Typography } from '@ui/atoms';
 import { NavbarHome } from '@ui/organisms/Navbar/NavbarHome';
 import { ROUTES_PATH } from '@src/routes/routesConstants';
+import { useUserContext } from '@context/user/userContext';
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 export function HomePage() {
+  const { user } = useUserContext();
+
   return (
     <div className="font-on min-h-screen flex flex-col items-center">
       <NavbarHome />
-      <div className="max-w-screen-sm flex flex-col justify-center items-center flex-1">
+      <div
+        className="max-w-screen-sm flex flex-col justify-center items-center flex-1"
+        dir="ltr"
+      >
         <Typography className="text-justify" size="body2">
-          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
-          ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و
-          کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی
-          در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
-          طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
-          الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این
-          صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
-          شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای
-          اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده
-          قرار گیرد.
+          The application is built using Python with the Django REST Framework
+          for the backend and React Vite for the frontend. The purpose of the
+          application is to implement an IPS service that works with Snort,
+          providing rule-based intrusion detection and prevention. The
+          application also integrates with Kibana for monitoring Snort and Zeek
+          logs. Additionally, the system includes OSSEC and IP tables for
+          enhanced security. The IDS (Intrusion Detection System) component is
+          implemented using Zeek, and its logs are displayed in Kibana. This
+          documentation outlines the architecture and key components of the
+          system.
         </Typography>
 
-        <Link to={ROUTES_PATH.login}>
+        <Link to={user ? ROUTES_PATH.dashboard : ROUTES_PATH.login}>
           <BaseButton
-            label="login"
+            label={user ? 'برو به داشبورد' : 'ورود'}
             className="mt-6 w-64"
             endIcon="ic:outline-login"
             size="lg"
