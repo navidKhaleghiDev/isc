ad5a1b83b9fddd8dd28e9b3249d71f92da7743bb
safari <sargashte118@gmail.com>
2023-07-05 14:07:04 +0330

    refactor: remove landing home and optimize no result

--- src/components/molecules/NoResult/index.tsx
+++ src/components/molecules/NoResult/index.tsx
@@ -1,10 +1,22 @@
 import { Typography } from '@ui/atoms';
 
-export function NoResult() {
+type PropsType = {
+  isPage?: boolean;
+  description?: string;
+};
+export function NoResult({ isPage, description }: PropsType) {
   return (
-    <div className="w-full min-h-full flex flex-col justify-center items-center">
+    <div
+      className={`w-full ${
+        isPage ? 'min-h-full' : 'h-64'
+      } flex flex-col justify-center items-center`}
+    >
       <img src="/not-found.jpg" alt="not found" />
-      <Typography>موردی یافت نشد</Typography>
+      {description ? (
+        <Typography>{description}</Typography>
+      ) : (
+        <Typography>موردی یافت نشد</Typography>
+      )}{' '}
     </div>
   );
 }
--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,19 +1,16 @@
 import { ROUTES_PATH } from '@src/routes/routesConstants';
 import { BaseButton } from '@ui/atoms/BaseButton';
+import { NoResult } from '@ui/molecules/NoResult';
 import { Link } from 'react-router-dom';
 
 function NotFoundPage() {
   return (
-    <div
-      dir="ltr"
-      className="p-16 h-screen flex flex-col justify-center items-center"
-    >
-      <h1 className="">Not Found</h1>
-      <p>Oops! page not found.</p>
+    <div className="p-16 h-screen flex flex-col justify-center items-center font-on">
+      <NoResult description="صفحه مورد نظر یافت نشد!" />
       <Link to={ROUTES_PATH.home}>
         <BaseButton
           className="mt-6"
-          label="go to home"
+          label="برو به خانه"
           type="default"
           size="lg"
         />
