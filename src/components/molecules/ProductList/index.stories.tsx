8757b3862b451b3989d01eb352b6105115bfbd64
a.doozande <a.doozande@npd-co.com>
2024-06-24 10:18:23 +0330

    feat: product list storybook (not yet complete)

--- src/components/molecules/ProductList/index.stories.tsx
+++ src/components/molecules/ProductList/index.stories.tsx
@@ -1,38 +1,34 @@
-import { Meta, StoryObj } from "@storybook/react/*";
-import { ProductList } from ".";
-import { ReactElement } from "react";
+import { ReactElement } from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { ProductList } from '.';
 
 const meta = {
-    title: 'molecules/ProductList',
-    component: ProductList,
-    parameters: {
-        layout: 'centered',
-        docs: {
-            description: {
-                component: `
+  title: 'molecules/ProductList',
+  component: ProductList,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: `
                 
                 `,
-            },
-        },
+      },
     },
-    tags: ['autodocs'],
-    args: {
-
-    },
-    argTypes: {},
-    decorators: [
-        (Story): ReactElement => (
-            <div dir="rtl" className="font-on">
-                <Story />
-            </div>
-        )
-    ],
-} satisfies Meta<typeof meta>;
+  },
+  tags: ['autodocs'],
+  args: {},
+  argTypes: {},
+  decorators: [
+    (Story): ReactElement => (
+      <div dir="rtl" className="font-on">
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof ProductList>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-
-}
+export const Default: Story = {};
