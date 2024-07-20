ea308c7c9175119c1232722af25bb321c02fab3b
a.doozande <a.doozande@npd-co.com>
2024-07-20 10:54:38 +0330

    redesign: change list of icons in storybook

--- src/components/atoms/BaseIcon/BaseIcon.stories.tsx
+++ src/components/atoms/BaseIcon/BaseIcon.stories.tsx
@@ -1,6 +1,119 @@
+import numberZero from '@iconify-icons/ph/number-zero';
+import numberOne from '@iconify-icons/ph/number-one';
+import numberTwo from '@iconify-icons/ph/number-two';
+import numberThree from '@iconify-icons/ph/number-three';
+import numberFour from '@iconify-icons/ph/number-four';
+import numberFive from '@iconify-icons/ph/number-five';
+import numberSix from '@iconify-icons/ph/number-six';
+import numberSeven from '@iconify-icons/ph/number-seven';
+import numberEight from '@iconify-icons/ph/number-eight';
+import numberNine from '@iconify-icons/ph/number-nine';
+import keyHole from '@iconify-icons/ph/keyhole';
+import x from '@iconify-icons/ph/x';
+import check from '@iconify-icons/ph/check';
+import caretDown from '@iconify-icons/ph/caret-down';
+import caretLeft from '@iconify-icons/ph/caret-left';
+import caretRight from '@iconify-icons/ph/caret-right';
+import caretUp from '@iconify-icons/ph/caret-up';
+import caretCircleUpDown from '@iconify-icons/ph/caret-circle-up-down';
+import warningCircle from '@iconify-icons/ph/warning-circle';
+import userCircle from '@iconify-icons/ph/user-circle';
+import userCircleMinus from '@iconify-icons/ph/user-circle-minus';
+import userCirclePlus from '@iconify-icons/ph/user-circle-plus';
+import dotsThree from '@iconify-icons/ph/dots-three';
+import info from '@iconify-icons/ph/info';
+import minusCircle from '@iconify-icons/ph/minus-circle';
+import cloud from '@iconify-icons/ph/cloud';
+import cloudX from '@iconify-icons/ph/cloud-x';
+import cloudWarning from '@iconify-icons/ph/cloud-warning';
+import cloudSlash from '@iconify-icons/ph/cloud-slash';
+import cloudArrowDown from '@iconify-icons/ph/cloud-arrow-down';
+import cloudArrowUp from '@iconify-icons/ph/cloud-arrow-up';
+import cloudCheck from '@iconify-icons/ph/cloud-check';
+import bugBeetle from '@iconify-icons/ph/bug-beetle';
+import key from '@iconify-icons/ph/key';
+import chartLine from '@iconify-icons/ph/chart-line';
+import bookmarkSimple from '@iconify-icons/ph/bookmark-simple';
+import folderSimple from '@iconify-icons/ph/folder-simple';
+import mouseSimple from '@iconify-icons/ph/mouse-simple';
+import devices from '@iconify-icons/ph/devices';
+import speakerHigh from '@iconify-icons/ph/speaker-high';
+import speakerNone from '@iconify-icons/ph/speaker-none';
+import power from '@iconify-icons/ph/power';
+import trashSimple from '@iconify-icons/ph/trash-simple';
+import uploadSimple from '@iconify-icons/ph/upload-simple';
+import mapPin from '@iconify-icons/ph/map-pin';
+import house from '@iconify-icons/ph/house';
+import houseSimple from '@iconify-icons/ph/house-simple';
+import monitor from '@iconify-icons/ph/monitor';
+import rss from '@iconify-icons/ph/rss';
+import mapTrifold from '@iconify-icons/ph/map-trifold';
+import envelopeSimple from '@iconify-icons/ph/envelope-simple';
+import newspaper from '@iconify-icons/ph/newspaper';
+import fingerprintSimple from '@iconify-icons/ph/fingerprint-simple';
+import article from '@iconify-icons/ph/article';
+import list from '@iconify-icons/ph/list';
+import chatTeardropText from '@iconify-icons/ph/chat-teardrop-text';
+import chatsTeardrop from '@iconify-icons/ph/chats-teardrop';
+import phone from '@iconify-icons/ph/phone';
+import navigationArrow from '@iconify-icons/ph/navigation-arrow';
+import bellSimple from '@iconify-icons/ph/bell-simple';
+import arrowClockwise from '@iconify-icons/ph/arrow-clockwise';
+import chartPie from '@iconify-icons/ph/chart-pie';
+import magnifyingGlass from '@iconify-icons/ph/magnifying-glass';
+import squaresFour from '@iconify-icons/ph/squares-four';
+import codeSimple from '@iconify-icons/ph/code-simple';
+import fileArrowDown from '@iconify-icons/ph/file-arrow-down';
+import fileArrowUp from '@iconify-icons/ph/file-arrow-up';
+import bellSimpleRinging from '@iconify-icons/ph/bell-simple-ringing';
+import timer from '@iconify-icons/ph/timer';
+import circlesFour from '@iconify-icons/ph/circles-four';
+import lockSimple from '@iconify-icons/ph/lock-simple';
+import lockSimpleOpen from '@iconify-icons/ph/lock-simple-open';
+import deviceMobileCamera from '@iconify-icons/ph/device-mobile-camera';
+import deviceTabletCamera from '@iconify-icons/ph/device-tablet-camera';
+import eye from '@iconify-icons/ph/eye';
+import listMagnifyingGlass from '@iconify-icons/ph/list-magnifying-glass';
+import thumbsUp from '@iconify-icons/ph/thumbs-up';
+import usersThree from '@iconify-icons/ph/users-three';
+import play from '@iconify-icons/ph/play';
+import computerTower from '@iconify-icons/ph/computer-tower';
+import image from '@iconify-icons/ph/image';
+import shield from '@iconify-icons/ph/shield';
+import shieldCheck from '@iconify-icons/ph/shield-check';
+import shieldWarning from '@iconify-icons/ph/shield-warning';
+import downloadSimple from '@iconify-icons/ph/download-simple';
+import gear from '@iconify-icons/ph/gear';
+import filmScript from '@iconify-icons/ph/film-script';
+import sticker from '@iconify-icons/ph/sticker';
+import megaphone from '@iconify-icons/ph/megaphone';
+import hardDrive from '@iconify-icons/ph/hard-drive';
+import hardDrives from '@iconify-icons/ph/hard-drives';
+import microphone from '@iconify-icons/ph/microphone';
+import handTap from '@iconify-icons/ph/hand-tap';
+import bellSlash from '@iconify-icons/ph/bell-slash';
+import slideshow from '@iconify-icons/ph/slideshow';
+import slidersHorizontal from '@iconify-icons/ph/sliders-horizontal';
+import lightbulb from '@iconify-icons/ph/lightbulb';
+import waveform from '@iconify-icons/ph/waveform';
+import globe from '@iconify-icons/ph/globe';
+import browser from '@iconify-icons/ph/browser';
+import translate from '@iconify-icons/ph/translate';
+import desktop from '@iconify-icons/ph/desktop';
+import deviceMobile from '@iconify-icons/ph/device-mobile';
+import addressBook from '@iconify-icons/ph/address-book';
+import columns from '@iconify-icons/ph/columns';
+import pencilSimple from '@iconify-icons/ph/pencil-simple';
+import user from '@iconify-icons/ph/user';
+import gearSix from '@iconify-icons/ph/gear-six';
+import signOut from '@iconify-icons/ph/sign-out';
+import calendarBlank from '@iconify-icons/ph/calendar-blank';
+import eyeSlash from '@iconify-icons/ph/eye-slash';
+import sun from '@iconify-icons/ph/sun';
+import moon from '@iconify-icons/ph/moon';
+
 import { BaseIcon } from './BaseIcon';
 import { IIconListStorybook, IconListStorybook } from './ListIconStorybook';
-import { IBaseIcon } from './types';
 
 /**
  *
@@ -10,22 +123,119 @@ import { IBaseIcon } from './types';
  *
  */
 const iconOptions = {
-  User: 'ph:user',
-  RoundLogin: 'ic:round-login',
-  UserPlus: 'iconoir:user-plus',
-  CalendarCheck: 'ph:calendar-check',
-  Cube: 'ph:cube',
-  Rows: 'ph:rows',
-  ArrowLeft: 'iconamoon:arrow-left-2-thin',
-  GearSix: 'ph:gear-six',
-  Monitor: 'ph:monitor',
-  Devices: 'ph:devices',
-  UsersThree: 'ph:users-three',
-  LogoutSharp: 'material-symbols:logout-sharp',
-  MoreVertical: 'jam:more-vertical',
-  BaselinePlus: 'ic:baseline-plus',
-  Bake: 'ep:back',
-  TrashSimple: 'ph:trash-simple',
+  NumberZero: numberZero,
+  NumberOne: numberOne,
+  NumberTwo: numberTwo,
+  NumberThree: numberThree,
+  NumberFour: numberFour,
+  NumberFive: numberFive,
+  NumberSix: numberSix,
+  NumberSeven: numberSeven,
+  NumberEight: numberEight,
+  NumberNine: numberNine,
+  KeyHole: keyHole,
+  X: x,
+  Check: check,
+  CaretDown: caretDown,
+  CaretLeft: caretLeft,
+  CaretRight: caretRight,
+  CaretUp: caretUp,
+  CaretCircleUpDown: caretCircleUpDown,
+  WarningCircle: warningCircle,
+  UserCircle: userCircle,
+  UserCircleMinus: userCircleMinus,
+  UserCirclePlus: userCirclePlus,
+  DotsThree: dotsThree,
+  Info: info,
+  MinusCircle: minusCircle,
+  Cloud: cloud,
+  CloudX: cloudX,
+  CloudWarning: cloudWarning,
+  CloudSlash: cloudSlash,
+  CloudArrowDown: cloudArrowDown,
+  CloudArrowUp: cloudArrowUp,
+  CloudCheck: cloudCheck,
+  BugBeetle: bugBeetle,
+  Key: key,
+  ChartLine: chartLine,
+  BookmarkSimple: bookmarkSimple,
+  FolderSimple: folderSimple,
+  MouseSimple: mouseSimple,
+  Devices: devices,
+  SpeakerHigh: speakerHigh,
+  SpeakerNone: speakerNone,
+  Power: power,
+  TrashSimple: trashSimple,
+  UploadSimple: uploadSimple,
+  MapPin: mapPin,
+  House: house,
+  HouseSimple: houseSimple,
+  Monitor: monitor,
+  RSS: rss,
+  MapTrifold: mapTrifold,
+  EnvelopeSimple: envelopeSimple,
+  Newspaper: newspaper,
+  FingerprintSimple: fingerprintSimple,
+  Article: article,
+  List: list,
+  ChatTeardropText: chatTeardropText,
+  ChatsTeardrop: chatsTeardrop,
+  Phone: phone,
+  NavigationArrow: navigationArrow,
+  BellSimple: bellSimple,
+  ArrowClockwise: arrowClockwise,
+  ChartPie: chartPie,
+  MagnifyingGlass: magnifyingGlass,
+  SquaresFour: squaresFour,
+  CodeSimple: codeSimple,
+  FileArrowDown: fileArrowDown,
+  FileArrowUp: fileArrowUp,
+  BellSimpleRinging: bellSimpleRinging,
+  Timer: timer,
+  CirclesFour: circlesFour,
+  LockSimple: lockSimple,
+  LockSimpleOpen: lockSimpleOpen,
+  DeviceMobileCamera: deviceMobileCamera,
+  DeviceTabletCamera: deviceTabletCamera,
+  Eye: eye,
+  ListMagnifyingGlass: listMagnifyingGlass,
+  ThumbsUp: thumbsUp,
+  UsersThree: usersThree,
+  Play: play,
+  ComputerTower: computerTower,
+  Image: image,
+  Shield: shield,
+  ShieldCheck: shieldCheck,
+  ShieldWarning: shieldWarning,
+  DownloadSimple: downloadSimple,
+  Gear: gear,
+  FilmScript: filmScript,
+  Sticker: sticker,
+  Megaphone: megaphone,
+  HardDrive: hardDrive,
+  HardDrives: hardDrives,
+  Microphone: microphone,
+  HandTap: handTap,
+  BellSlash: bellSlash,
+  Slideshow: slideshow,
+  SlidersHorizontal: slidersHorizontal,
+  Lightbulb: lightbulb,
+  Waveform: waveform,
+  Globe: globe,
+  Browser: browser,
+  Translate: translate,
+  Desktop: desktop,
+  DeviceMobile: deviceMobile,
+  AddressBook: addressBook,
+  Columns: columns,
+  PencilSimple: pencilSimple,
+  User: user,
+  GearSix: gearSix,
+  SignOut: signOut,
+  CalendarBlank: calendarBlank,
+  EyeSlash: eyeSlash,
+  Sun: sun,
+  Moon: moon,
 };
 
 const meta = {
@@ -81,6 +291,7 @@ export function ListIcon({
   className,
 }: Omit<IIconListStorybook, 'icons'>) {
   const iconValue = Object.values(iconOptions);
+  const iconKey = Object.keys(iconOptions);
   return (
     <IconListStorybook
       icons={iconValue}
@@ -88,45 +299,14 @@ export function ListIcon({
       size={size}
       hoverColor={hoverColor}
       className={className}
+      title={iconKey}
     />
   );
 }
 
 ListIcon.args = {
-  color: 'teal',
-  size: 'xxxl',
-  hoverColor: 'default',
-  className: '',
-};
-
-/**
- * This function is defined by default to pass these properties to the main component.
- * @param args {icon, color, size, hoverColor, className}
- * @returns
- */
-export function Default({
-  icon,
-  color,
-  size,
-  hoverColor,
-  className,
-}: IBaseIcon) {
-  const iconValue = iconOptions[icon as keyof typeof iconOptions];
-  return (
-    <BaseIcon
-      icon={iconValue}
-      color={color}
-      size={size}
-      hoverColor={hoverColor}
-      className={className}
-    />
-  );
-}
-
-Default.args = {
-  icon: 'User',
-  color: 'teal',
-  size: 'xxxl',
+  color: 'neutral',
+  size: 'md',
   hoverColor: 'default',
   className: '',
 };
