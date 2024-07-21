eacf2f01b52f079b3f12aff5ffb069c813db0195
m.talebi <mahita.contact@gmail.com>
2024-07-21 06:28:32 -0400

    feat: add cardProduct component

---
+++ src/components/molecules/CardProduct/index.tsx
@@ -0,0 +1,33 @@
+import { Card, Typography } from '@ui/atoms';
+
+type PropsType = {
+  label: string;
+  value: string | number;
+  className?: string;
+};
+
+export function CardProduct({ label, value, className }: PropsType) {
+  return (
+    <Card className={`flex justify-start items-center mb-6 ${className}`}>
+      <div className="flex flex-col justify-end items-start w-full">
+        <Typography
+          color="neutral_dark"
+          size="body4"
+          className="mb-[6px] font-semibold"
+        >
+          {label}
+        </Typography>
+        <Typography
+          color="neutral"
+          size="body5"
+          weight="normal"
+          className="
+            !isValueLeft && 'mr-auto'
+          } bg-neutral-100 w-full px-2.5 rounded-lg"
+        >
+          {value}
+        </Typography>
+      </div>
+    </Card>
+  );
+}
