29a1fa0d9efca10df104f766c0644384823756c3
n.khaleghi <navidKhaleghi669@gamil.com>
2024-07-22 13:40:56 +0330

    reafactor: dropDown redesign add

--- src/pages/Services/Rules/index.tsx
+++ src/pages/Services/Rules/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { BaseButton } from '@ui/atoms';
+import { BaseButton, Dropdown } from '@ui/atoms';
+import { useForm } from 'react-hook-form';
 import { RulesList } from '@ui/molecules/Rules/RulesList';
 import { WithPermission, EUserRole } from '@src/helper/hoc/withPermission';
 import { SearchInput } from '@ui/atoms/Inputs/SearchInput';
@@ -9,10 +10,16 @@ import { ButtonState } from './types';
 function RulesPageCP() {
   const [activeButton, setActiveButton] = useState<ButtonState>('suggest');
   const [search, setSearch] = useState('');
+  const { control } = useForm();
 
   const handleOnSearch = (value: string) => {
     setSearch(value);
   };
+  const dropDownOptions = [
+    { value: '1', label: 'مرتب سازی قوانین' },
+    { value: '2', label: 'به تعویق افتاده' },
+    { value: '3', label: 'در حال انجام ' },
+  ];
 
   const handleClickTab = (tab: ButtonState) => {
     setActiveButton(tab);
@@ -20,23 +27,23 @@ function RulesPageCP() {
 
   return (
     <div className="w-full flex flex-col h-full p-16">
-      <div className="p-4 flex justify-start items-center gap-4">
+      <form className="p-4 flex justify-start items-center gap-4">
         <SearchInput onChange={handleOnSearch} value={search} />
-        {/* <Dropdown
-          options={[
-            { value: '1', label: 'انجام شده' },
-            { value: '2', label: 'به تعویق افتاده' },
-            { value: '3', label: 'در حال انجام ' },
-          ]}
-          onSelect={() => console.log('drop')}
-        /> */}
+        <Dropdown
+          options={dropDownOptions}
+          placeHolder="مرتب سازی قوانین"
+          control={control}
+          id="listener_id"
+          name="rulesSortOptions"
+          size="lg"
+        />
         <BaseButton
           label="جستجوی قوانین"
           size="xl"
           type={activeButton === 'suggest' ? 'default' : 'shadow'}
           onClick={() => handleClickTab('suggest')}
         />
-      </div>
+      </form>
 
       {/* {activeButton === 'all' && (
         <div className="w-1/3 mt-4">
