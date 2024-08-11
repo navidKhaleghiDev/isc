54bcb662cc38447b1473cb951403af218f9c1dd7
n.khaleghi <navidKhaleghi669@gamil.com>
2024-08-11 18:03:56 +0330

    style: change backButoon tooltip

--- src/pages/Dashboard/BoxDashboard/index.tsx
+++ src/pages/Dashboard/BoxDashboard/index.tsx
@@ -42,8 +42,9 @@ export function BoxDashboard({
   return (
     <Card
       border
+      rounded="lg"
       shadow="sm"
-      className={`flex items-center px-2 py-4 md:p-7 mb-5 col-span-2 ${className}`}
+      className={`flex items-center sm:rounded-[1.25rem] px-2 py-1 sm:py-4 md:p-7 mb-5 col-span-2 ${className}`}
     >
       <>
         {showIcon()}
