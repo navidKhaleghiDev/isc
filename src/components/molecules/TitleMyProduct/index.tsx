3bd1eb3cc017f0e6c75f7e6b3b819a68af8d1fdc
m.talebi <mahita.contact@gmail.com>
2024-09-08 03:24:13 -0400

    fix: remove h6

--- src/components/molecules/TitleMyProduct/index.tsx
+++ src/components/molecules/TitleMyProduct/index.tsx
@@ -7,7 +7,7 @@ type TitleMyProductProps = {
 export function TitleMyProduct({ title }: TitleMyProductProps) {
   return (
     <div className="flex items-center">
-      <Typography color="teal" size="h6" className="w-[6rem]">
+      <Typography color="teal" size="h4" className="w-[6rem]">
         {title}
       </Typography>
     </div>
