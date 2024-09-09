25251448791704c3c0cc79387483934c5b4c1c4d
m.talebi <mahita.contact@gmail.com>
2024-09-09 03:48:44 -0400

    fix: remove additional them

--- src/components/atoms/BaseButton/styles.ts
+++ src/components/atoms/BaseButton/styles.ts
@@ -8,18 +8,14 @@ export const baseButtonStyles = cva(
       type: {
         default: `${PALLET.BUTTON_COLOR.TEAL}`,
         inactive: `${PALLET.BUTTON_COLOR.TEAL} opacity-40`,
-        shadow: `${PALLET.BUTTON_COLOR.SHADOW}`,
-        secondary: `${PALLET.BUTTON_COLOR.YELLOW}`,
         red: `${PALLET.BUTTON_COLOR.RED}`,
         neutral: `${PALLET.BUTTON_COLOR.NEUTRAL}`,
-        tealLink: `text-teal-600 hover:bg-neutral-200 rounded-none`,
         tertiary: `${PALLET.BUTTON_COLOR.TERTIARY}`,
       },
       size: {
         sm: SIZE.BUTTON.SMALL,
         md: SIZE.BUTTON.MEDIUM,
         lg: SIZE.BUTTON.LARGE,
-        xl: SIZE.BUTTON.X_LARGE,
       },
       fullWidth: {
         true: 'w-full',
@@ -35,10 +31,8 @@ export const baseButtonStyles = cva(
 export const iconInButtonStyles = cva('fill-current ', {
   variants: {
     type: {
-      default: `${PALLET.BUTTON_COLOR.TEAL} hover:bg-red-100`,
+      default: `${PALLET.BUTTON_COLOR.TEAL}`,
       inactive: `${PALLET.BUTTON_COLOR.NEUTRAL} border border-neutral-600`,
-      shadow: `bg-white text-teal-600 shadow-lg`,
-      secondary: `${PALLET.BUTTON_COLOR.YELLOW}`,
       red: `${PALLET.BUTTON_COLOR.RED} `,
       tealLink: ``,
       noBg: ``,
@@ -63,26 +57,16 @@ export const iconButtonStyles = cva(
     variants: {
       color: {
         teal: `${PALLET.BUTTON_COLOR.TEAL}`,
-        tealDark: 'bg-teal-500',
-        tealNoBg: 'text-teal-600',
         redNoBg: `text-red-500 hover:text-red-600 active:text-red-700 disabled:opacity-40 
                   dark:text-red-300 dark:hover:text-red-400 dark:active:text-red-500 dark:disabled:text-red-100`,
         neutral: `${PALLET.BUTTON_COLOR.NEUTRAL}`,
         neutralNoBg: `text-neutral-500 hover:text-neutral-600 active:text-neutral-900 disabled:opacity-40 
                       dark:text-neutral-300 dark:hover:text-neutral-200 dark:active:text-neutral-100 dark:disabled:text-[#4B5563]`,
-        neutralLight: 'bg-neutral-100 text-neutral-600',
-        yellow: 'bg-yellow-600 text-neutral-100',
-        red: 'bg-red-100 text-red-600',
-        white: 'bg-white text-neutral-900',
         default: 'text-neutral-600',
       },
       size: {
         sm: 'h-7 w-7',
         md: 'h-10 w-10',
-        lg: 'h-6 w-6',
-        xl: 'h-8 w-8',
-        xxl: 'h-10 w-10',
-        xxxl: 'h-16 w-16',
         default: 'h-fit w-fit',
       },
     },
