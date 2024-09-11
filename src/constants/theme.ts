eb448cb913c1319d149ddbca9fc3c7d06df1f08b
mahdi jafarabadi <m.jafarabadi@npd-co.ir>
2024-09-11 07:19:47 -0400

    remove : pallet changes

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,46 +1,14 @@
 export const PALLET = {
   BG_COLOR: {
-    RED_EXTRA_LIGHT: 'bg-red-100',
-    RED_LIGHT: 'bg-red-200',
-    RED: 'bg-red-300',
-    RED_MAIN: 'bg-red-400',
-    RED_DARK: 'bg-red-500',
-    RED_EXTRA_DARK: 'bg-red-600',
-
-    TEAL_EXTRA_LIGHT: 'bg-teal-100',
-    TEAL_LIGHT: 'bg-teal-200',
-    TEAL: 'bg-teal-300',
-    TEAL_MAIN: 'bg-teal-400',
-    TEAL_DARK: 'bg-teal-500',
-    TEAL_EXTRA_DARK: 'bg-teal-600',
-
-    BLUE_EXTRA_LIGHT: 'bg-blue-100',
-    BLUE_LIGHT: 'bg-blue-200',
-    BLUE: 'bg-blue-300',
-    BLUE_MAIN: 'bg-blue-400',
-    BLUE_DARK: 'bg-blue-500',
-    BLUE_EXTRA_DARK: 'bg-blue-600',
-
-    NEUTRAL_EXTRA_LIGHT: 'bg-gray-100',
-    NEUTRAL_LIGHT: 'bg-gray-200',
-    NEUTRAL: 'bg-gray-300',
-    NEUTRAL_MAIN: 'bg-gray-400',
-    NEUTRAL_DARK: 'bg-gray-500',
-    NEUTRAL_EXTRA_DARK: 'bg-gray-600',
-
-    YELLOW_EXTRA_LIGHT: 'bg-yellow-100',
-    YELLOW_LIGHT: 'bg-yellow-200',
-    YELLOW: 'bg-yellow-300',
-    YELLOW_MAIN: 'bg-yellow-400',
-    YELLOW_DARK: 'bg-yellow-500',
-    YELLOW_EXTRA_DARK: 'bg-yellow-600',
-
-    PURPLE_EXTRA_LIGHT: 'bg-purple-100',
-    PURPLE_LIGHT: 'bg-purple-200',
-    PURPLE: 'bg-purple-300',
-    PURPLE_MAIN: 'bg-purple-400',
-    PURPLE_DARK: 'bg-purple-500',
-    PURPLE_EXTRA_DARK: 'bg-purple-600',
+    TEAL_LIGHT: 'bg-teal-100',
+    TEAL: 'bg-teal-200 dark:bg-teal-300', // #f0fdfa
+    TEAL_DARK: 'bg-teal-500  ', // #f0fdfa
+    RED_LIGHT: 'bg-red-200 dark:bg-red-300', // #fee2e2
+    RED: 'bg-red-400',
+    YELLOW: 'bg-yellow-100 dark:bg-yellow-300', // #f2e9dc
+    NEUTRAL_LIGHT: 'bg-neutral-100 dark:bg-neutral-300', // #f5f5f5
+    NEUTRAL: 'bg-neutral-200',
+    NEUTRAL_DARK: 'bg-neutral-500',
   },
   TEXT_COLOR: {
     TEAL: 'text-teal-500 dark:text-teal-500',
@@ -90,15 +58,15 @@ export const SIZE = {
     BODY6: 'text-xs',
   },
   BUTTON: {
-    SMALL: `h-10 w-[95px] p-2 text-sm`,
-    MEDIUM: 'h-10 w-40 p-2',
-    LARGE: 'h-10 w-[190px] p-2',
-    X_LARGE: 'h-10 w-full p-2 ',
+    SMALL: 'h-10 w-16 py-1.5 px-3 text-sm',
+    MEDIUM: 'h-10 w-24 py-2 px-5',
+    LARGE: 'h-10 w-40 py-3 px-6 ',
+    X_LARGE: 'h-10 w-48 py-3 px-6 ',
   },
   INPUT: {
-    SMALL: 'h-6 w-[160px]',
-    MEDIUM: 'h-10 w-[255px]',
-    LARGE: 'h-10 w-[350px]',
-    X_LARGE: 'h-10 w-full',
+    SMALL: 'h-6 w-52',
+    MEDIUM: 'h-10 w-56',
+    LARGE: 'h-10 w-64',
+    X_LARGE: 'h-10 w-[60rem]',
   },
 };
