e0ecf65c6afeca8e4174004f417ea65f386622a1
m.talebi <mahita.contact@gmail.com>
2024-09-11 08:23:49 -0400

    feat : add js doc & add dark mode

--- src/components/atoms/BaseSwitch/BaseSwitch.tsx
+++ src/components/atoms/BaseSwitch/BaseSwitch.tsx
@@ -1,9 +1,31 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable no-nested-ternary */
 import { Controller } from 'react-hook-form';
 import { Typography } from '@ui/atoms/Typography';
 import { BaseSwitchProps } from './types';
 import { baseSwitchStyles } from './styles';
+
+/**
+ * BaseSwitch component renders a customizable toggle switch with optional labels and controlled/uncontrolled states.
+ * It supports integration with `react-hook-form` when the `control` prop is provided, and allows for custom onChange handling.
+ *
+ * @component
+ *
+ * @param {Object} props - The properties for the BaseSwitch component.
+ * @param {'small' | 'medium' | 'large'} props.size - Defines the size of the switch.
+ * @param {string} [props.label] - Optional label displayed next to the switch.
+ * @param {string} props.name - The name of the switch input, used as its identifier.
+ * @param {Object} [props.control] - `react-hook-form` control object for controlled forms.
+ * @param {Object} [props.rules] - Validation rules used with `react-hook-form`.
+ * @param {boolean} [props.ltrLabel] - If true, label is aligned to the left (LTR); otherwise, it's aligned to the right.
+ * @param {boolean} [props.defaultValue] - Default value for the controlled state (used with `react-hook-form`).
+ * @param {(checked: boolean) => void} [props.pureOnChange] - Callback function to handle onChange events.
+ * @param {boolean} [props.pureValue] - External value for uncontrolled switch state.
+ * @param {boolean} [props.defaultChecked] - Default checked state for the uncontrolled switch.
+ * @param {string} [props.pureError] - Error message displayed when validation fails.
+ * @param {boolean} [props.disabled=false] - If true, disables the switch and prevents user interaction.
+ *
+ * @returns {JSX.Element} The BaseSwitch component.
+ */
 
 export function BaseSwitch({
   size,
@@ -18,7 +40,9 @@ export function BaseSwitch({
   defaultChecked,
   pureError,
   disabled = false,
-}: BaseSwitchProps<any>) {
+}: BaseSwitchProps<any>): JSX.Element {
+  const translateClass = size === 'small' ? 'translate-x-4' : 'translate-x-6';
+
   return control ? (
     <Controller
       name={name}
@@ -57,15 +81,15 @@ export function BaseSwitch({
               <span
                 className={`${baseSwitchStyles({
                   size,
-                })} ${field.value ? 'bg-teal-500' : 'bg-neutral-200'}`}
+                })} ${
+                  field.value
+                    ? 'bg-teal-500 dark:bg-teal-400'
+                    : 'bg-neutral-200 dark:bg-neutral-800'
+                }`}
               >
                 <span
                   className={`dot size-4 rounded-full bg-white duration-200 ${
-                    field.value
-                      ? size === 'small'
-                        ? 'translate-x-4'
-                        : 'translate-x-6'
-                      : ''
+                    field.value ? translateClass : ''
                   }`}
                 />
               </span>
@@ -103,15 +127,15 @@ export function BaseSwitch({
         <span
           className={`${baseSwitchStyles({
             size,
-          })} ${pureValue ? 'bg-teal-500' : 'bg-neutral-200'}`}
+          })} ${
+            pureValue
+              ? 'bg-teal-500 dark:bg-teal-400'
+              : 'bg-neutral-200 dark:bg-neutral-800'
+          }`}
         >
           <span
             className={`dot size-4 rounded-full bg-white duration-200 ${
-              pureValue
-                ? size === 'small'
-                  ? 'translate-x-4'
-                  : 'translate-x-6'
-                : ''
+              pureValue ? translateClass : ''
             }`}
           />
         </span>
