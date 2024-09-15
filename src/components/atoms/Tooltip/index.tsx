5544d55652a48ebc5bb79efcced466700ef9f7e3
fateme-shabani <shabanifateme22@gmail.com>
2024-09-15 15:18:21 +0330

    add new position in jsDoc

--- src/components/atoms/Tooltip/index.tsx
+++ src/components/atoms/Tooltip/index.tsx
@@ -13,7 +13,7 @@ import { ToolTipProps } from './types';
  * @param {ToolTipPrps} props - The props for ToolTip component.
  * @param {React.ReactNode} props.children - The child elements that trigger the tooltip on hover.
  * @param {string} props.tooltip - The content to be displayed inside the tooltip.
- * @param {'top'|'right'|'bottom'|'left'} props.position - The position of the tooltip relative to the child elements.
+ * @param {'top'|'right'|'bottom'|'left'|'topStart'|'topEnd'|'bottomStart'|'bottomEnd'} props.position - The position of the tooltip relative to the child elements.
  *
  * @returns {JSX.Element} The rendered ToolTip component.
  */
