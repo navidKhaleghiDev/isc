2672da8726e236f93caf1d6733b63b6ce7e05466
navidXlg <navidjoker669@gmail.com>
2024-09-23 08:40:08 +0330

    style: add responsive rule to size

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import { useEffect, useState } from 'react';
-// import { useForm } from 'react-hook-form';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  // const { control } = useForm({
-  //   defaultValues: {
-  //     firstName: '',
-  //     lastName: '',
-  //   },
-  // });
 
   useEffect(() => {
     if (darkMode) {
@@ -27,7 +20,6 @@ function App() {
       <button type="button" onClick={() => setDarkMode(!darkMode)}>
         Toggle Dark Mode
       </button>
-      {/* <BaseCheckBox control={control} id="n" name="n" dir="rtl" label="nanna" /> */}
       {/* Your components here */}
     </div>
   );
