d0f1aedf20ba0a82b61d6e5cfad6c01ac5d563b2
m.talebi <mahita.contact@gmail.com>
2024-09-25 01:32:03 -0400

    fix: fixed storybook

--- src/components/atoms/BaseSwitch/ BaseSwitch.stories.tsx
+++ src/components/atoms/BaseSwitch/ BaseSwitch.stories.tsx
@@ -40,7 +40,6 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     name: 'switch',
-    defaultChecked: false,
     size: 'responsive',
     disabled: false,
   },
