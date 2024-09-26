f220478af2cfdf7222485a0d249a4535969f2698
فاطمه شعبانی <shabani.f@npdco.local>
2024-09-26 16:13:30 +0000

    feat:Add the ControllerMultipleBaseSlider component

---
+++ src/components/atoms/Slider/MultipleBaseSlider/Controller/ControllerMultipleBaseSlider.tsx
@@ -0,0 +1,63 @@
+import { Controller, useForm } from "react-hook-form";
+
+import { MultipleBaseSlider } from "../MultipleBaseSlider"; 
+import { MultipleBaseSliderProps } from "../../types";
+
+export interface FormValue {
+  min: number;
+  max: number;
+}
+
+export interface ControllerMultipleBaseSliderProps extends Omit<MultipleBaseSliderProps, 'onChange'> {
+  initialMin: number;
+  initialMax: number;
+}
+
+export function ControllerMultipleBaseSlider(props: ControllerMultipleBaseSliderProps) {
+  const { min, max, initialMin, initialMax, hiddenLabel } = props;
+
+  const { control, setValue } = useForm<FormValue>({
+    defaultValues: { min: initialMin, max: initialMax },
+  });
+
+  return (
+    <>
+      <Controller
+        name="min"
+        control={control}
+        render={({ field }) => (
+          <MultipleBaseSlider
+            min={min}
+            max={max}
+            initialMin={field.value}
+            initialMax={initialMax} 
+            hiddenLabel={hiddenLabel}
+            onChange={({ min, max }) => {
+              setValue('min', min);
+              setValue('max', max);
+              field.onChange(min); 
+            }}
+          />
+        )}
+      />
+      <Controller
+        name="max"
+        control={control}
+        render={({ field }) => (
+          <MultipleBaseSlider
+            min={min}
+            max={max}
+            initialMin={initialMin}
+            initialMax={field.value}
+            hiddenLabel={hiddenLabel}
+            onChange={({ min, max }) => {
+              setValue('min', min);
+              setValue('max', max);
+              field.onChange(max);
+            }}
+          />
+        )}
+      />
+    </>
+  );
+}
